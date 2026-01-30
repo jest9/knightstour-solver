bdc5386a2d3c4f19d63977398d0cf21708dd5e66
jest9 <max.radini@outlook.com>
2026-01-30 13:27:51 +0000

    added simple draw of grid

--- grid.py
+++ grid.py
@@ -8,5 +8,18 @@ print("gridding up....")
 
 pygame.init()
 
-sample_surface = pygame.Surface((400, 400))
-sample_surface.fill((255, 255, 255))
+sample_surface = pygame.display.set_mode((400,400))
+
+#color = (255,255,255)
+
+#pygame.draw.rect(sample_surface, color, pygame.Rect(30, 30, 60, 60))
+
+x = 1
+while True:
+    pygame.display.flip()
+    x = x +0
+    if x == 0:
+        break
+    else:
+        pass
+    
