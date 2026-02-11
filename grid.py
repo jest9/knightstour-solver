a71da1a72986643af29784893d41c7183730282c
jest9 <max.radini@outlook.com>
2026-02-11 14:10:34 +0000

    added temporary grid draw / recording coordinates

--- grid.py
+++ grid.py
@@ -6,9 +6,33 @@ import pygame
 
 print("gridding up....")
 
+
+# grid size # 
+size = 8
+
 pygame.init()
 
-sample_surface = pygame.display.set_mode((400,400))
+sample_surface = pygame.display.set_mode((600,600))
+
+
+# grid handling
+
+grid_coordinates = [] # for storing grid coordinates
+
+def draw_grid(surface, size):
+    width, height = surface.get_size()
+    cell_width = width // size  # calculates width and height of each cell
+    cell_height = height // size
+
+    for i in range(size):   # loops drawing grid lines
+        for j in range(size):
+            rect = pygame.Rect(j * cell_width, i * cell_height, cell_width, cell_height)
+            pygame.draw.rect(surface, (255, 255, 255), rect, 1) # draw grid lines
+            grid_coordinates.append((j * cell_width, i * cell_height))
+    
+    for i in range(grid_coordinates.__len__()):
+        print(grid_coordinates[i])
+
 
 #color = (255,255,255)
 
@@ -16,8 +40,9 @@ sample_surface = pygame.display.set_mode((400,400))
 
 x = 1
 while True:
+    draw_grid(sample_surface, size)
     pygame.display.flip()
-    x = x +0
+    x = x - 1
     if x == 0:
         break
     else:
