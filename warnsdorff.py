b2650524a3924ca8c49c1d127e7daf8453ff5430
jest9 <max.radini@outlook.com>
2026-02-27 13:09:55 +0000

    adding warnsdorff heuristic

---
+++ warnsdorff.py
@@ -0,0 +1,5 @@
+"""
+adding warnsdorff's heuristic to the knight's tour algorithm, finds next move with least number of onward moves
+"""
+
+# combine with squirrel and cull algorithm to find better solutions, also add backtracking to find complete tours
