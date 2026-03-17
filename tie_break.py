a420e8921eba988650a64b86d2999f94f8aa5f4f
jest9 <167774811+jest9@users.noreply.github.com>
2026-03-17 00:46:31 +0000

    added find center of grid

--- tie_break.py
+++ tie_break.py
@@ -1,3 +1,20 @@
 """
-tiebreak algorithm
+tiebreak algorithm (roth tiebreak)
 """
+
+import random
+import time
+
+def roth(move_list, grid):
+
+
+    # find central point
+    if len(grid) % 2 != 0:
+        middleIndex = (len(grid) - 1)/2
+        midIndex = int(middleIndex)
+        print(grid[midIndex])
+
+        # finish tomorrow
+
+
+    return random.choice(move_list)
