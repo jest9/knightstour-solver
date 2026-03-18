c5889bbacc76d5cdd98c0b45f50d25daa495c4bc
jest9 <max.radini@outlook.com>
2026-03-18 14:01:49 +0000

    added furthest euclidean distance tie-breaking

--- tie_break.py
+++ tie_break.py
@@ -2,11 +2,7 @@
 tiebreak algorithm (roth tiebreak)
 """
 
-import random
-import time
-
 def roth(move_list, grid):
-
 
     # find central point
     if len(grid) % 2 != 0:
@@ -14,7 +10,21 @@ def roth(move_list, grid):
         midIndex = int(middleIndex)
         print(grid[midIndex])
 
-        # finish tomorrow pls
+    y = 0
+    for i in move_list:
+        # find distance from central point
+        distance = ((i[0] - grid[midIndex][0]) ** 2 + (i[1] - grid[midIndex][1]) ** 2) ** 0.5 # get euclidean distance from central point
+        print("distance from central point for move", i, "is: ", distance)
+        if y == 0:
+            y = distance
+            best_move = i
+        elif distance > y:
+            y = distance
+            best_move = i
+
+            # add for even grid size
+            
 
 
-    return random.choice(move_list)
+    print("best move is: ", best_move, "with distance: ", y)
+    return best_move
