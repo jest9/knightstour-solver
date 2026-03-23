2a8828ac96d59f1591ce1cd0f71db5e1a77dfe5b
jest9 <max.radini@outlook.com>
2026-03-23 16:38:35 +0000

    refactor draw_grid function to streamline grid setup and remove direct input handling

--- grid.py
+++ grid.py
@@ -5,23 +5,6 @@ Docstring for grid draw with pygame
 import pygame
 import logic
 
-
-print("gridding up....")
-
-size = int(input("provide grid size: "))
-
-
-# setup window and icon
-
-icon_path = "icons/chess.jpeg"
-programIcon = pygame.image.load(icon_path)
-
-# grid size # 
-
-pygame.init()
-
-sample_surface = pygame.display.set_mode((800,800))
-
 # grid handling
 
 grid_coordinates = [] # for storing grid coordinates
@@ -30,6 +13,13 @@ def draw_grid(surface, size):
     width, height = surface.get_size()
     cell_width = width // size  # calculates width and height of each cell
     cell_height = height // size
+
+    # headless mode (cli)
+
+    icon_path = "icons/chess.jpeg"
+    programIcon = pygame.image.load(icon_path)
+
+        # pygame visual mode
 
     for i in range(size):   # loops drawing grid lines
         for j in range(size):
@@ -40,9 +30,9 @@ def draw_grid(surface, size):
 
             grid_coordinates.append((j * cell_width, i * cell_height)) # store grid coordinates in list
 
-    logic.grid_logic(size, surface, grid_coordinates) # call logic engine
+    logic.grid_logic(size, grid_coordinates)
 
 
-draw_grid(sample_surface, size)
+#draw_grid(sample_surface, size)
 
     
