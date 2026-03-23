9f80a434071d544ec10d72a7002427de08937e44
jest9 <max.radini@outlook.com>
2026-03-23 16:38:49 +0000

    refactor init.py to improve structure and logic flow for grid initialization

--- init.py
+++ init.py
@@ -2,8 +2,21 @@
 Docstring for init
 """
 
-import grid
+from config import f, size
+from logic import grid_logic
+from grid import draw_grid
 
 print("welcome to knightstour")
 
-grid()
+print(f)
+
+if f == 1:
+    grid_logic(size)
+
+elif f == 0:
+    from config import sample_surface
+    draw_grid(sample_surface, size)
+
+
+
+
