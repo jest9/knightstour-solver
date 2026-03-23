8cde3770a9e678dadf38abc29085d594c0d961e9
jest9 <max.radini@outlook.com>
2026-03-23 16:39:07 +0000

    refactor logic.py to enhance grid and move logic functions, improve statistics reporting, and streamline headless mode handling

--- logic.py
+++ logic.py
@@ -5,20 +5,31 @@ logic docstring
 from draw import draw_pattern
 from tie_break import roth
 import itertools
+from config import headless_option
+if headless_option == "0":
+    from config import size, sample_surface
 
 
-def grid_logic(var1, surface, grid_coordinates):
-    grid = list(itertools.product(range(var1), range(var1)))
-    usedmoves = move_logic(surface, var1, grid, grid_coordinates)
+
+def grid_logic(size, grid_coordinates=False):
+
+    grid = list(itertools.product(range(size), range(size)))
+
+    if grid_coordinates != False:
+        usedmoves = move_logic(grid, grid_coordinates)
+    else:
+        usedmoves = move_logic(grid, grid_coordinates=False, headless=True)
     return usedmoves
 
 
-def move_logic(surface, var1, grid, grid_coordinates):
+def move_logic(grid, grid_coordinates=False, headless=False):
 
     knight_moves = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
     valid_moves = []
     used_moves = set() # used moves stored in a set for O(1) lookup
     warns_move = []
+    tied_moves = 0
+    random_tied_moves_increment = 0
     
     # starting point
     start = grid[0]
@@ -26,7 +37,10 @@ def move_logic(surface, var1, grid, grid_coordinates):
     used_moves.add(start)
     point = start
 
-    draw_pattern(surface, var1, start, grid_coordinates)
+    if (headless == True):
+        pass
+    else:
+        draw_pattern(sample_surface, size, start, grid_coordinates)
 
     # find valid moves
 
@@ -40,7 +54,30 @@ def move_logic(surface, var1, grid, grid_coordinates):
 
         if not valid_moves: # no more valid moves
             x = 0
-            print("moves made =", len(used_moves), "out of", len(grid))
+
+            print("\n ending tour...")
+
+            # stat formatting
+            
+            stat_moves_total = len(grid)
+            stat_moves_made = len(used_moves)
+            stat_success_rate = stat_moves_made/stat_moves_total * 100
+            stat_tied_moves = tied_moves
+            stat_random_tied_moves = random_tied_moves_increment
+            stat_percentage_tied_moves = tied_moves/stat_moves_made * 100
+
+            # stats start
+
+            print("Statistics:")
+            print(f"Total moves: {stat_moves_total}")
+            print(f"Moves made: {stat_moves_made}")
+            print(f"Success rate: {stat_success_rate:.2f}%")
+            print(f"Tied moves: {stat_tied_moves}")
+            print(f"Randomly tie broken: {stat_random_tied_moves}")
+            print(f"Percentage of tied moves: {stat_percentage_tied_moves:.2f}%")
+
+            # stats end
+
             return
 
         j = 0
@@ -77,17 +114,25 @@ def move_logic(surface, var1, grid, grid_coordinates):
         # heuristic ending
 
         if len(tie_list) > 1:
-            warns_move = roth(tie_list, grid)
+            warns_move, random_value = roth(tie_list, grid)
+            tied_moves += 1
+            if random_value == 1:
+                random_tied_moves_increment += 1
 
         tie_list.clear()
 
         valid_moves.clear()
         point = warns_move
 
-        print(warns_move)
+        #print(warns_move)
         used_moves.add(warns_move)
 
-        draw_pattern(surface, var1, warns_move, grid_coordinates)
+        print(f"\r{len(used_moves)/len(grid) * 100:.1f} percent done", end="", flush=True)
+
+        if (headless == True):
+            pass
+        else:
+            draw_pattern(sample_surface, size, warns_move, grid_coordinates)
 
 
         
