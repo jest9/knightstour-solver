bd30c8c254d26896596994e7ae7cf0f369ff6aa3
jest9 <max.radini@outlook.com>
2026-03-26 13:05:11 +0000

    use cProfile to run grid_logic for performance profiling in headless mode

--- init.py
+++ init.py
@@ -4,11 +4,12 @@ Docstring for init
 
 from config import f, size
 from logic import grid_logic
+import cProfile
 
 print("\n")
 
 if f == 1:                      # method to decouple the solver from pygame, allows for headless
-    grid_logic(size)
+    cProfile.run('grid_logic(size)')
 
 elif f == 0:
     from grid import draw_grid
