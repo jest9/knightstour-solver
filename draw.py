4473ca2e6cd05945a7120063df827f9f2a2c5b24
jest9 <max.radini@outlook.com>
2026-03-26 14:47:46 +0000

    update rectangle color in draw_pattern function for improved visibility

--- draw.py
+++ draw.py
@@ -15,7 +15,7 @@ def draw_pattern(surface, size, value, grid_coordinates):
     row_value = x
     column_value = y
 
-    pygame.draw.rect(surface, (173, 216, 230), pygame.Rect(grid_coordinates[column_value][0], grid_coordinates[row_value][0], cell_width, cell_height))
+    pygame.draw.rect(surface, (160, 0, 255), pygame.Rect(grid_coordinates[column_value][0], grid_coordinates[row_value][0], cell_width, cell_height))
         
     pygame.display.flip()
     pygame.event.pump()
