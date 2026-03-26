cbc4bda1347f5518adc3ee371d086b7294979849
jest9 <max.radini@outlook.com>
2026-03-26 14:48:06 +0000

    refactor roth function to improve tie-breaking logic and initialize variables correctly

--- tie_break.py
+++ tie_break.py
@@ -3,6 +3,7 @@ tiebreak algorithm (roth tiebreak)
 """
 
 import random
+import time
 
 
 def roth(move_list, size):
@@ -16,20 +17,31 @@ def roth(move_list, size):
 
 
     y = 0
+    tie_list = []
+    c == 0
+    
     for i in move_list:
         # find distance from central point
         distance = ((i[0] - center[0]) ** 2 + (i[1] - center[1]) ** 2) ** 0.5 # get euclidean distance from central point
-        #print("distance from central point for move", i, "is: ", distance)
+        
         if y == 0:
             y = distance
             best_move = i
-        elif distance > y:
+            tie_list.append(i)
+        elif y < distance:
+            tie_list.clear()
+            tie_list.append(i)
             y = distance
             best_move = i
-        elif distance == y:
-            best_move = random.choice(move_list)
-            
-    return best_move, c
+        elif y == distance:
+            tie_list.append(i)
+
+    if len(tie_list) > 1:
+        best_move = random.choice(tie_list)
+        c = 1
+        return best_move, c
+    else:
+        return best_move, c
 
 
 
