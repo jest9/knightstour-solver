cd49786cd129ec90aa98a24e90328429eba92f11
jest9 <max.radini@outlook.com>
2026-04-23 15:44:06 +0100

    moved stats into own method for readability

--- logic.py
+++ logic.py
@@ -3,6 +3,7 @@ logic docstring
 """
 
 from tie_break import roth
+from stats import stats
 import itertools
 from config import headless_option, size
 if headless_option == "0":
@@ -58,28 +59,7 @@ def move_logic(grid, grid_coordinates=False, headless=False):
 
             print("\nEnding tour...")
 
-            # stat formatting
-            
-            stat_moves_total = len(grid)
-            stat_moves_made = len(used_moves)
-            stat_success_rate = stat_moves_made/stat_moves_total * 100
-            stat_tied_moves = tied_moves
-            stat_random_tied_moves = random_tied_moves_increment
-            stat_percentage_tied_moves = tied_moves/stat_moves_made * 100
-
-            # stats start
-
-            print("Statistics:")
-            print(f"Total moves: {stat_moves_total}")
-            print(f"Moves made: {stat_moves_made}")
-            print(f"Success rate: {stat_success_rate:.2f}%")
-            print(f"Tied moves: {stat_tied_moves}")
-            print(f"Randomly tie broken: {stat_random_tied_moves}")
-            print(f"Percentage of tied moves: {stat_percentage_tied_moves:.2f}%")
-
-            print("\033[0m")
-
-            # stats end
+            stats(len(grid), len(used_moves), tied_moves, random_tied_moves_increment)
 
             break
 
@@ -129,15 +109,15 @@ def move_logic(grid, grid_coordinates=False, headless=False):
         valid_moves.clear()
         point = warns_move
 
-        #print(warns_move)
         used_moves.add(warns_move)
-
 
         if (headless == True):
             pass
         else:
             from draw import draw_pattern
             draw_pattern(sample_surface, size, warns_move, grid_coordinates)
+
+
 
 
         
---
+++ stats.py
@@ -0,0 +1,18 @@
+"""
+docstring for stats for tour
+"""
+
+def stats(stat_moves_total, stat_moves_made, stat_tied_moves, stat_random_tied_moves):
+
+    stat_success_rate = stat_moves_made/stat_moves_total * 100
+    stat_percentage_tied_moves = stat_tied_moves/stat_moves_made * 100
+
+    print("Statistics:")
+    print(f"Total moves: {stat_moves_total}")
+    print(f"Moves made: {stat_moves_made}")
+    print(f"Success rate: {stat_success_rate:.2f}%")
+    print(f"Tied moves: {stat_tied_moves}")
+    print(f"Randomly tie broken: {stat_random_tied_moves}")
+    print(f"Percentage of tied moves: {stat_percentage_tied_moves:.2f}%")
+
+    print("\033[0m")
